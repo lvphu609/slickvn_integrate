176fdfa38c3f5df5ba2058a70401ff75b361d0de
xinhhuynh <xinhhuynh@innoria.com>
2013-12-26 11:39:19 +0700

    fix api search_restaurant_multi_field

--- application/models/common/common_model.php
+++ application/models/common/common_model.php
@@ -1040,7 +1040,24 @@ class Common_model extends CI_Model{
 	    foreach($utf8 as $ascii=>$uni) $str = preg_replace("/($uni)/i",$ascii,$str);
 	return $str;
 	}
-    
+        
+        public function array_merge_recursive_distinct ( array &$array1, array &$array2 ){
+            
+	  $merged = $array1;
+
+	  foreach ( $array2 as $key => &$value )
+	  {
+		if ( is_array ( $value ) && isset ( $merged [$key] ) && is_array ( $merged [$key] ) )
+		{
+		  $merged [$key] = array_merge_recursive_distinct ( $merged [$key], $value );
+		}
+		else
+		{
+		  $merged [$key] = $value;
+		}
+	  }
+        }
+
 }
 
 ?>
