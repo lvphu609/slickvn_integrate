333b957e41b8328a38a64cb50a7b6c6eb1b8e25d
lvphu609 <lvphu609@gmail.com>
2013-12-29 22:32:08 +0700

    commit normal

--- application/controllers/home_controller.php
+++ application/controllers/home_controller.php
@@ -13,7 +13,6 @@ class Home_controller extends CI_Controller {
     $this->load->model('restaurant/restaurant_apis');
     $this->load->model('common/common_apis');
     $this->load->model('user/user_apis');
-    $this->load->model('common/common_model');
     $this->load->library('session');
     $this->info_user   = $this->session->userdata('info_user');
     
@@ -118,10 +117,11 @@ class Home_controller extends CI_Controller {
       $item_url=  base_url();
       $footer_content_info_item = 'application/views/home/content/footer_content_info_item.tpl';      
       $template_footer_content_info_item = new Templater($footer_content_info_item);                  
-      $website_info_list = $this->common_model->get_website_info_list();      
+      $website_info_list = $this->common_apis->get_website_info_list(1);   
+      var_dump($website_info_list);
       $result_website_info_list = "";
       $i = 0;
-      foreach ($website_info_list as $value){
+      foreach ($website_info_list['Results'] as $value){
         $template_footer_content_info_item->item_url = base_url().'index.php/home_controller/webinfo?f='.$value['code'];                
         $template_footer_content_info_item->item_name = $value['name'];
         $result_website_info_list = $result_website_info_list.$template_footer_content_info_item->parse();
